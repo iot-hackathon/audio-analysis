8723d1b0b958f412d9deeb9b3ef334bcccfe599e
Sugandha Agrawal <agrawals@de.ibm.com>
2016-03-10 20:54:55 +0100

    Add thread

--- audio.py
+++ audio.py
@@ -10,6 +10,10 @@ import wave
 import audioop
 import signal
 import sys
+import time
+import threading
+#import numpy as np
+#import math
 
 #Iot service dependencies
 import os,json,math,time,logging
@@ -35,19 +39,39 @@ RATE = 44100
 
 INPUT_BLOCK_TIME = 0.05
 INPUT_FRAMES_PER_BLOCK = int(RATE * INPUT_BLOCK_TIME)
+t = []
+
+class Worker(threading.Thread):
+    def __init__(self, name, id):
+        threading.Thread.__init__(self)
+        self.myName = name
+        self.myID = id
+    def run(self):
+        print (self.myID)
+        streamCollected = listen(self.myID)
+        while True:
+            try:
+                data = streamCollected.read(CHUNK)
+                rms  = audioop.rms(data, 2)
+                if rms > 5000:
+                    timeStamp = int(round(time.time()*1000))
+                    pushData(rms,client,timeStamp)
+            except IOError as e:
+                print( "Error recording: %s" % (e) )
+                break
 
 def find_input_device(pa):
     device_index = None
-    for i in range( pa.get_device_count() ):
+    for i in range(pa.get_device_count() ):
         devinfo = pa.get_device_info_by_index(i)
         print( "Found device %d: %s" % (i, devinfo["name"]) )
 
-        for keyword in ["headset"]:
-            if keyword in devinfo["name"].lower():
-                print( "Matching device: %d - %s"%(i,devinfo["name"]) )
-                device_index = i
-            if device_index != None:
-                return device_index
+        # for keyword in ["usb"]:
+        #     if keyword in devinfo["name"].lower():
+        #         print( "Matching device: %d - %s"%(i,devinfo["name"]) )
+        #         device_index = i
+        #     if device_index != None:
+        #         return device_index
 
     if device_index == None:
         print( "No preferred input found; using default input device." )
@@ -56,17 +80,18 @@ def find_input_device(pa):
 
 def signal_handler(signal, frame):
     print('You pressed Ctrl+C!')
+    for i in range(4):
+        t[i].exit()
     sys.exit(0)
 
-def listen():
+def listen(id):
     device_index = find_input_device(pa)
-
     stream = pa.open(
         format             = FORMAT,
         channels           = CHANNELS,
         rate               = RATE,
         input              = True,
-        input_device_index = device_index,
+        input_device_index = id,
         frames_per_buffer  = CHUNK
     )
 
@@ -89,27 +114,18 @@ def initialize():
     except ibmiotf.ConnectionException as e:
         print(e)
 
-def pushData(result):
-    jsondata = {"Microphone" : { "stream" : str(result) }}
-    client=initialize()
+def pushData(result , client, timeStamp):
+    jsondata = {"Microphone" : { "stream" : str(result) }, "Time" :{"timestamp" : timeStamp}, "Id" :{"microphoneId": 1}}
     client.publishEvent("status","json",jsondata)
 
 if __name__ == "__main__":
     pa = pyaudio.PyAudio()
-    streamCollected = listen()
-
+    client=initialize()
     signal.signal(signal.SIGINT, signal_handler)
+    for i in range(4):
+        t.append(Worker("Thread", i))
+        t[i].start()
 
-    while True:
-        try:
-            data = streamCollected.read(CHUNK)
-            rms  = audioop.rms(data, 2)
-            if rms > 5000:
-                pushData(rms)
-        except IOError as e:
-            print( "Error recording: %s" % (e) )
-            break
-
-    stream.stop_stream()
-    stream.close()
-    pa.terminate()
+    # stream.stop_stream()
+    # stream.close()
+    # pa.terminate()
